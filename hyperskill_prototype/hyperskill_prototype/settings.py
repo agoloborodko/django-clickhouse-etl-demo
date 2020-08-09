0966be2c819e5f5713c2dcd3b1fa42ff2da12143
a.goloborodko <isitreallyimportant7@yandex.ru>
2020-08-10 01:46:46 +0300

    Переместил style.css

--- hyperskill_prototype/hyperskill_prototype/settings.py
+++ hyperskill_prototype/hyperskill_prototype/settings.py
@@ -128,7 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join('static'), )
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
