e6cac12cb1687c2ce98ddde9e706fd621345a625
a.goloborodko <isitreallyimportant7@yandex.ru>
2020-08-09 13:09:51 +0300

    Добавил PrototypeUser и PrototypeEvent в модель данных

--- hyperskill_prototype/educational_platform/models.py
+++ hyperskill_prototype/educational_platform/models.py
@@ -10,3 +10,30 @@ class PrototypeTask(models.Model):
         return str(self.pk)
 
 
+class PrototypeUser(models.Model):
+    """
+    Этот класс описывает не настоящих пользователей с логинами, полями
+    и security issues, а их массово генерируемые прототипы.
+    Использовать встроенную в django пользовательскую модель
+    для случайцно сгенерированных пользователей будет дорого.
+    """
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    join_date = models.DateTimeField(auto_now_add=True)
+    registration_date = models.DateTimeField(null=True)
+    is_guest = models.PositiveSmallIntegerField()
+    # TODO: реализовать проверки is_guest: if true, then name, email, registration_date is null
+    # TODO: реализовать boolean-like поведение поля is_guest
+
+
+class PrototypeEvent(models.Model):
+    ACTIONS = [
+        (0, 'Увидеть задачу'),
+        (1, 'Отправить попытку решения'),
+        (2, 'Решить задачу')
+    ]
+    time = models.DateTimeField()
+    action_id = models.SmallIntegerField(choices=ACTIONS)
+    target_id = models.ForeignKey(PrototypeTask, on_delete=models.PROTECT)
+    user_id = models.ForeignKey(PrototypeUser, on_delete=models.PROTECT)
+
