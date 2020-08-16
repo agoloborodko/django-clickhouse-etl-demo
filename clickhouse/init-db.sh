5b82ed6b8b9fbec7059699c2634a2e664fde69e6
a.goloborodko <isitreallyimportant7@yandex.ru>
2020-08-17 01:11:45 +0300

    Исправил ошибку

--- clickhouse/init-db.sh
+++ clickhouse/init-db.sh
@@ -4,6 +4,6 @@ set -e
 clickhouse client -n <<-EOSQL
     CREATE DATABASE IF NOT EXISTS prototype_dwh;
     CREATE USER IF NOT EXISTS prototype_admin IDENTIFIED BY 'pass';
-    ALTER USER prototype_admin SETTINGS allow_introspection_functions=1
+    ALTER USER prototype_admin SETTINGS allow_introspection_functions=1;
     GRANT ALL ON *.* TO prototype_admin WITH GRANT OPTION;
 EOSQL
