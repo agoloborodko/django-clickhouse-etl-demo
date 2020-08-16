7e0fd2e7e0029300c86a92061f2fa81cc9961317
a.goloborodko <isitreallyimportant7@yandex.ru>
2020-08-17 01:13:05 +0300

    Юорюсь с ошибкой Introspection functions are disabled

--- clickhouse/init-db.sh
+++ clickhouse/init-db.sh
@@ -4,6 +4,7 @@ set -e
 clickhouse client -n <<-EOSQL
     CREATE DATABASE IF NOT EXISTS prototype_dwh;
     CREATE USER IF NOT EXISTS prototype_admin IDENTIFIED BY 'pass';
-    ALTER USER prototype_admin SETTINGS allow_introspection_functions=1;
+    ALTER USER default SETTINGS allow_introspection_functions=1;
     GRANT ALL ON *.* TO prototype_admin WITH GRANT OPTION;
+    ALTER USER default SETTINGS allow_introspection_functions=0;
 EOSQL
