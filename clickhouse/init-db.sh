86f0a66ba8aa938057ab1a398d079a343e63a190
a.goloborodko <isitreallyimportant7@yandex.ru>
2020-08-17 00:50:51 +0300

    Борюсь с ошибкой  Database docker already exists.

--- clickhouse/init-db.sh
+++ clickhouse/init-db.sh
@@ -2,8 +2,8 @@
 set -e
 
 clickhouse client -n <<-EOSQL
-    CREATE DATABASE docker;
-    CREATE USER docker_admin IDENTIFIED BY 'pass';
-    GRANT ALL ON docker TO docker_admin WITH GRANT OPTION;
+    CREATE DATABASE prototype_dwh;
+    CREATE USER prototype_admin IDENTIFIED BY 'pass';
+    GRANT ALL ON prototype_dwh TO prototype_admin WITH GRANT OPTION;
 EOSQL
 #    CREATE TABLE docker.docker (x Int32) ENGINE = Log;
