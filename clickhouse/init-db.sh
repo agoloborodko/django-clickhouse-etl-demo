f912cb49b469aa4439612a4961e95719b382d077
a.goloborodko <isitreallyimportant7@yandex.ru>
2020-08-17 01:07:39 +0300

    Борюсь с ошибкой database is already exists

--- clickhouse/init-db.sh
+++ clickhouse/init-db.sh
@@ -2,8 +2,8 @@
 set -e
 
 clickhouse client -n <<-EOSQL
-    CREATE DATABASE prototype_dwh;
-    CREATE USER prototype_admin IDENTIFIED BY 'pass';
+    CREATE DATABASE IF NOT EXISTS prototype_dwh;
+    CREATE USER IF NOT EXISTS prototype_admin IDENTIFIED BY 'pass';
     ALTER USER prototype_admin SETTINGS allow_introspection_functions=1
     GRANT ALL ON *.* TO prototype_admin WITH GRANT OPTION;
 EOSQL
